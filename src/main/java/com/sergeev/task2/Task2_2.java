79b3c95ec7cb336ec3c5bfaf6f1c715bf913c2f2
Daniil Siarheyeu <daniil.siarheyeu@itrexgroup.lan>
2021-03-25 20:53:26 +0300

    task 2_2

---
+++ src/main/java/com/sergeev/task2/Task2_2.java
@@ -0,0 +1,18 @@
+package main.java.com.sergeev.task2;
+
+/**
+ * Task return true if all passed values are different otherwise false.
+ */
+public class Task2_2 {
+    @FunctionalInterface
+    interface TernaryIntPredicate {
+        boolean test(int a, int b, int c);
+    }
+
+    private static final TernaryIntPredicate allValuesAreDifferentPredicate = (x, y, z) -> x != y && x != z && y != z;
+
+    public static void main(String[] args) {
+        System.out.println(allValuesAreDifferentPredicate.test(1,1,1)); //false result
+        System.out.println(allValuesAreDifferentPredicate.test(1,2,3)); //true result
+    }
+}
