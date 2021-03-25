384d770b67d54257ef1d2df58c6364d5c76dd5ab
Daniil Siarheyeu <daniil.siarheyeu@itrexgroup.lan>
2021-03-25 14:09:07 +0300

    task 1_4

---
+++ src/main/java/com/sergeev/task1/Task1_4.java
@@ -0,0 +1,25 @@
+package main.java.com.sergeev.task1;
+
+import java.util.stream.LongStream;
+
+/**
+ * Task accepts two long arguments as a range borders and calculates (returns) production of all numbers in this range.
+ */
+public class Task1_4 {
+
+    interface Calculator {
+        long calculateProductionOfNumbersInRange(long startRange, long endRange);
+    }
+
+    public static void main(String[] args) {
+        long firstNumber = Long.parseLong(args[0]);
+        long secondNumber = Long.parseLong(args[1]);
+        Task1_4 task1_3 = new Task1_4();
+        Calculator calculator = (a, b) -> LongStream.rangeClosed(a, b).boxed().reduce(1L, (x, y) -> x*y);
+        System.out.println(task1_3.process(firstNumber,secondNumber, calculator));
+    }
+
+    public long process(long firstNumber, long secondNumber, Calculator calculator) {
+        return calculator.calculateProductionOfNumbersInRange(firstNumber,secondNumber);
+    }
+}
