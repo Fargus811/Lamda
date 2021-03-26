319770223499fd4841a125f74307e9b8c905fa31
Daniil Siarheyeu <daniil.siarheyeu@itrexgroup.lan>
2021-03-26 16:41:15 +0300

    task 3_1 prime number

---
+++ src/main/java/com/sergeev/task3/Task3_1.java
@@ -0,0 +1,18 @@
+package main.java.com.sergeev.task3;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+/**
+ * Task should check is the input value is prime number.
+ */
+public class Task3_1 {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(3));
+    }
+
+    public static boolean isPrime(final long number) {
+        return number > 1 && LongStream.range(2, number).noneMatch(i -> number%i==0);
+    }
+}
